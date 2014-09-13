98496ed8a4e5759da073ed1c074073b8e6398fca
Mark Koopman <markmontymark@yahoo.com>
2014-09-13 09:43:33 -0700

    Adding dlambda tests from http://letoverlambda.com/index.cl/guest/chap5.html

--- clisp/t/dlambda.lisp
+++ clisp/t/dlambda.lisp
@@ -7,16 +7,46 @@
 			 (dlambda
 				 (:inc () (incf count))
 				 (:dec () (decf count)))))
-; Factorial function defined recursively where `self' refers to the alambda function
+
+(setf (symbol-function 'bigger-count-test)
+    (let ((count 0))
+      (dlambda
+        (:value () count)
+        (:reset () (setf count 0))
+        (:inc (n) (incf count n))
+        (:dec (n) (decf count n))
+        (:bound (lo hi)
+          (setf count
+            (min hi
+                 (max lo
+                      count)))))))
 (let 
  ((a (count-test :inc))
   (b (count-test :inc))
   (c (count-test :dec))
-  (d (count-test :dec)))
+  (d (count-test :dec))
+  (e (bigger-count-test :inc 5))
+  (f (bigger-count-test :dec 3)))
 
+ ; count-test
  (lisp-unit:assert-equal a 1)
  (lisp-unit:assert-equal b 2)
  (lisp-unit:assert-equal c 1)
- (lisp-unit:assert-equal d 0)))
+ (lisp-unit:assert-equal d 0)
+
+ ; bigger-count-test
+ (lisp-unit:assert-equal e 5)
+ (lisp-unit:assert-equal f 2)
+ 
+ (bigger-count-test :reset)
+
+ (lisp-unit:assert-equal (bigger-count-test :value) 0)
+
+ (bigger-count-test :bound 1 3)
+
+ (lisp-unit:assert-equal (bigger-count-test :value) 1)
+ (bigger-count-test :inc 5)
+ (bigger-count-test :bound 1 3)
+ (lisp-unit:assert-equal (bigger-count-test :value) 3)))
 
 (lisp-unit:write-tap (lisp-unit:run-tests :all))
